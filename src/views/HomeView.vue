b3e2c665f20a99f764bb14fb5b803d629025ad49
zaid0813 <zaid.hussain@aventior.com>
2022-05-17 23:50:27 +0530

    initial commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SignUp/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Login from '../components/auth/Login.vue'
+import SignUp from '../components/auth/SignUp.vue'
+
 
 export default {
   name: 'HomeView',
+  data() {
+    return {
+      
+    }
+  },
+
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    Login,
+    SignUp
+  },
 }
 </script>
